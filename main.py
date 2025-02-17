f095dda4c3653bf23842ebea543559027435513d
Bo Lin <learldu91@gmail.com>
2025-02-17 16:35:54 +0800

    增加註解跟openpyxl寫法

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import openpyxl
 
 #創建一個新的Excel工作簿
-workbook = openpyxl.Workbook()
+workbook = openpyxl.Workbook()  #workbook = Workbook()
 sheet = workbook.active
 
 #設置標題行
-sheet['A1'] = 'Product Name'
-sheet['B1'] = 'Sale Price'
-sheet['C1'] = 'Quantitiy Price'
+sheet['A1'] = 'Product Name'        #sheet['A1] = 'Product Name'
+sheet['B1'] = 'Sale Price'          #sheet['B1'] = 'Sale Price'
+sheet['C1'] = 'Quantitiy Sold'     #sheet['C1'] = 'Quantitiy Sold'
 
 #添加數據
 data = [
@@ -18,7 +18,7 @@ data = [
 ]
 
 #把矩陣後面的資料夾在first row後面
-for row in data:
+for row in data:                    #將data中的每一行數據逐行添加到Excel工作表中
     sheet.append(row)
 
 #保存工作簿
