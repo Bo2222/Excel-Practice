66d4b7a23bb1255b55bf4099bc9f8ad88c324ebd
Bo Lin <learldu91@gmail.com>
2025-02-18 11:20:44 +0800

    openpyxl和pandas去讀數據並獲取最大值

--- read.py
+++ read.py
@@ -9,4 +9,13 @@ data = []
 for row in sheet.iter_rows(values_only=True):
     data.append(list(row))
 
-print(data)
+#print(data)
+
+#顯示數據
+for row in data:
+    print(row)
+
+#獲取特定列的最大值（假設'Sale Price'在第二列）
+sale_prices = [row[1] for row in data[1:]] #跳過標題行(['Product Name', 'Sale Price', 'Quantitiy Sold'])
+max_sale_price = max(sale_prices)           #透過實作演算法(搜尋)去獲取最大值
+print(f'Max Sale Price is {max_sale_price}')
--- read2.py
+++ read2.py
@@ -3,4 +3,9 @@ import pandas as pd
 #讀取Excel文件並轉換成DataFrame
 df = pd.read_excel('example.xlsx')
 
+#顯示DataFrame的內容
 print(df)
+
+#獲取某一列的最大值
+max_value = df['Sale Price'].max()              #熊貓處理二維數據的專業可以快速獲取最大值
+print(f'Max Sale Price is {max_value}')
