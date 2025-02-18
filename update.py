45f3fbce8e10e65c65cce85023aef1692f29ca9e
Bo Lin <learldu91@gmail.com>
2025-02-18 11:37:00 +0800

    增加在不知道要修改值的特定位置，修改數值的程式碼

--- update.py
+++ update.py
@@ -6,6 +6,15 @@ sheet = workbook.active
 
 #修改特定單元的數據
 sheet['B2'] = 27.00     #將Widget A的價格從25.5改為27.00
+#可以這麼寫是因為表格簡單，可以一眼就看到要修改的位置是B2，但如果哪天表格欄位對調，就不能這樣寫了
+#要透過層層回區勸尋找
+
+#尋找'Widget A'的位置並修其價格
+for row in sheet.iter_rows(min_row = 2):    #從第二行開始，跳過標題行
+    if row[0].value == 'Widget A':          #假設第一列是產品名稱
+        row[1].value = 27.00                #修改價格
+        break                               #找到後跳出迴圈
+#在不知道特定值位置，openpyxl要遍歷整個工作表
 
 #保存修改後的工作簿
 workbook.save('example_modified_openpyxl.xlsx')
--- update2.py
+++ update2.py
@@ -5,6 +5,8 @@ df = pd.read_excel('example.xlsx')
 
 #修改特定單元的值（假設'Product Name'在第一列，'Sale Price'在第二列）
 df.loc[df['Product Name'] == 'Widget A', 'Sale Price'] = 27.00  #將Widget A的價格從25.5改為27.00
+#pandas透過條件過濾快速找到並修改數據
+#Pandas提供更高效和簡潔的數據操作方法，特別在處理大型數據集時
 
 #保存修改後的DataFrame到Excel文件
 df.to_excel('example_modified_pandas.xlsx', index=False)
