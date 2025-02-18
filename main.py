fdb68a48c36f6c379f029e8c784111e274eb236c
Bo Lin <learldu91@gmail.com>
2025-02-18 11:11:37 +0800

    嘗試建立excel檔案

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ sheet['C1'] = 'Quantitiy Sold'     #sheet['C1'] = 'Quantitiy Sold'
 
 #添加數據
 data = [
-    ['Widget A', 25.50, 100]
-    ['Widget B', 15.75, 200]
-    ['Widget C', 30.00, 150]
-    ['Widget D', 10.00, 300]
+    ['Widget A', 25.5, 100],
+    ['Widget B', 15.75, 200],
+    ['Widget C', 30.0, 150],
+    ['Widget D', 10.0, 300]
 ]
 
 #把矩陣後面的資料夾在first row後面
 for row in data:                    #將data中的每一行數據逐行添加到Excel工作表中
     sheet.append(row)
+    #print(row)                   #把row輸出
 
 #保存工作簿
-workbook.save('example.xlst')
+workbook.save('example.xlsx')       #workbook.save('example.xlsx')
 
